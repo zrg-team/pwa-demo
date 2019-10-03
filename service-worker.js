b28ecaf6abf63be308b613d7275ab6c5c635dbcd
hungdt <hungdt>
2019-10-03 13:27:59 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e74b9ea0650b2fb74e376eae506a50b4.js"
+  "/pwa-demo/precache-manifest.442a00028f6d93908bd78bfdf1144fc1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,34 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pwa-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
+
+importScripts('https://www.gstatic.com/firebasejs/4.2.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/4.2.0/firebase-messaging.js')
+
+/*
+ * Now initialize firebase app in the servie worker.
+ */
+firebase.initializeApp({
+  apiKey: 'AIzaSyC-TdnrW92tPP3_n7oo8netffqH5aJF_0o',
+  authDomain: 'game-store-zrg-team.firebaseapp.com',
+  databaseURL: 'https://game-store-zrg-team.firebaseio.com',
+  projectId: 'game-store-zrg-team',
+  storageBucket: 'game-store-zrg-team.appspot.com',
+  messagingSenderId: '790266601902'
+})
+
+var messaging = firebase.messaging()
+
+messaging.setBackgroundMessageHandler(function (payload) {
+  var notificationTitle = payload.data.title
+  var notificationOptions = {
+    body: payload.data.body,
+    icon: payload.data.icon
+  }
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions)
+})
