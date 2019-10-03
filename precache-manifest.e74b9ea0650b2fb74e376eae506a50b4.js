c931ac3b027900ac00d5f2e21a1fef8576eb5092
hungdt <hungdt>
2019-10-03 13:15:42 +0700

    Updates

---
+++ precache-manifest.e74b9ea0650b2fb74e376eae506a50b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9a68b7077cffe77b134a5e333cb15f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3df1b7c365993a3c4c9",
+    "url": "/static/css/main.84bf7061.chunk.css"
+  },
+  {
+    "revision": "86bf53a901f9584f8353",
+    "url": "/static/js/2.5868d529.chunk.js"
+  },
+  {
+    "revision": "b3df1b7c365993a3c4c9",
+    "url": "/static/js/main.bd64b07c.chunk.js"
+  },
+  {
+    "revision": "bd291511bf598bcc14a1",
+    "url": "/static/js/runtime-main.e3f636bf.js"
+  }
+]);
